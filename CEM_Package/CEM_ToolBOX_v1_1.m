94617ee5cbb2d063c705d4efa78e64507fd0a196
ArgenitTech <72024687+ArgenitTech@users.noreply.github.com>
2020-09-28 19:10:20 +0300

    Add files via upload

---
+++ CEM_Package/CEM_ToolBOX_v1_1.m
@@ -0,0 +1,29 @@
+function [] = CEM_ToolBOX_v1_1(); 
+
+
+
+% this GUI was developed by Krishnan Padmanabhan at the Crick-Jacobs Center in the Salk Institute for the lab of 
+% Fred H. Gage (Laboratory of Genetics) and was used in the manuscript Kerman et al. ______ as part of the myelin
+% analysis tool kit.  
+% 
+% All functions are detailed below - and any questions can be sent to krishnan@salk.edu -
+% These tools are provided as part of the fair use license.  Any use of them for scientific publication should include
+% the references 
+% 
+% K. Padmanabhan, W.F. Eddy, J.C. Crowley. A novel algorithm for optimal image thresholding of biological data. 2010.  
+% Journal of Neuroscience Methods 193(2):380-4.
+% 
+% and
+% 
+% Kerman et al. ______
+% 
+
+
+%this toolbox calls 2 gui panels, one of these panels is for thresholding
+%of images, while the other does image processing and dilation.  
+
+
+
+%MyelinOverlapCalculatorV1
+
+ CEMDilateExcludeAnalysisV1_4
